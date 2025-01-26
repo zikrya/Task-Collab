403c5ca7f524b8b3b16690e1400a920103d80a4c
zikrya <zikrya8@gmail.com>
2025-01-26 16:15:05 -0500

    feat: local/prod setup

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,7 +1,9 @@
-// src/lib/socket.ts
 import { io, Socket } from "socket.io-client";
 
-const socket: Socket = io("https://websocket-patient-waterfall-3880.fly.dev/");
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:8008";
+
+const socket: Socket = io(SOCKET_URL);
+
 socket.on("connect", () => {
   console.log("Connected to WebSocket server", socket.id);
 });
