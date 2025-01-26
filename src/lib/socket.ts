57de16c2fc8ebe15cd1974692d341875e5470668
zikrya <zikrya8@gmail.com>
2025-01-26 16:53:26 -0500

    clean up

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -3,10 +3,6 @@ import { io, Socket } from "socket.io-client";
 const SOCKET_URL =
   process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:8008";
 
-if (typeof window !== "undefined") {
-  console.log("Socket URL (Frontend):", SOCKET_URL);
-}
-
 const socket: Socket = io(SOCKET_URL);
 
 socket.on("connect", () => {
